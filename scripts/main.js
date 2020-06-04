328e35b3714d0704362f35f519ddaf933ae5f798
DeltaNedas <deltanedas@gmail.com>
2020-06-04 22:31:46 +0100

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+var ui = require("ui-lib/library");
+
+const control = {
+	boosting: false
+};
+this.global.boostControl = control;
+
+ui.addTable("side", "boost-control", table => {
+	table.background(Tex.buttonTrans);
+
+	const button = table.addButton("Not Boosting", run(() => {
+		control.boosting = !control.boosting;
+		button.setText(control.boosting ? "Boosting" : "Not Boosting");
+	})).width(120).get();
+});
+
+ui.once(() => {
+	Events.on(EventType.Trigger.update, run(() => {
+		Vars.player.isBoosting = control.boosting;
+	}));
+});
