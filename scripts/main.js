e683f8c76067d1d41e17b9c884fb9d32e4bf34fe
DeltaNedas <deltanedas@gmail.com>
2020-07-03 04:00:37 +0100

    1.2.0: update to ui lib 1.8.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,20 @@
+/*
+	Copyright (c) DeltaNedas 2020
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 var ui = require("ui-lib/library");
 
 const control = {
@@ -15,8 +32,6 @@ ui.addTable("side", "boost-control", table => {
 	})).width(120).get();
 });
 
-ui.once(() => {
-	Events.on(EventType.Trigger.update, run(() => {
-		Vars.player.isBoosting = control.boosting;
-	}));
-});
+Events.on(EventType.Trigger.update, run(() => {
+	Vars.player.isBoosting = control.boosting;
+}));
